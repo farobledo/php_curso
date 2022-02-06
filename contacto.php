7442a91ec96261a078f0bed19cf67ec9bd254159
Fabian Andres Robledo Olave <83982603+farobledo@users.noreply.github.com>
2022-02-06 15:05:51 -0500

    cambios2022

--- contacto.php
+++ contacto.php
@@ -1,29 +1,58 @@
 <?php include('template/cabecera.php') ?>
+<link rel="stylesheet" href="css/style.css">
 
-<div class="card text-center text-primary">
+<section>
+          <div class="container">
+          <div class="card text-center">
   <div class="card-header">
-    <h1>Contactame</h1>
+    <h1>Contacto</h1>
   </div>
   <div class="card-body">
     <h5 class="card-title">Special title treatment</h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
   <div class="card-footer text-muted">
     2 days ago
   </div>
 </div>
+          </div>
+     </section>
 
-<span class="border border-primary">Orario de Atencion</span>
-<span class="border border-secondary">Telefono</span>
-<span class="border border-success">Dirrecion</span>
-<span class="border border-danger">Referencia</span>
-<span class="border border-warning">Hoja de vidad</span>
-<span class="border border-info">Estudios</span>
-<span class="border border-light">Lenguaje de programacion</span>
-<span class="border border-dark">Cursos</span>
-<span class="border border-white">Descricion</span>
 
-<a href="index.php" class="btn btn-primary">Home</a>
+     <!-- CONTACT -->
+     <section id="contact">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-6 col-sm-12">
+                         <form id="contact-form" role="form" action="" method="post">
+                              <div class="col-md-12 col-sm-12">
+                                   <input type="text" class="form-control" placeholder="Nombre Completo" name="name" required>
+                    
+                                   <input type="email" class="form-control" placeholder="Correo" name="email" required>
+
+                                   <textarea class="form-control" rows="6" placeholder="Tell us about your message" name="message" required></textarea>
+                              </div>
+
+                              <div class="col-md-4 col-sm-12">
+                                   <input type="submit" class="form-control" name="send message" value="Enviar">
+                              </div>
+
+                         </form>
+                    </div>
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="contact-image">
+                              <img src="img/contacto2.jfif" width="500" alt="">
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>             
+
+
+
 
 <?php include('template/pie.php')?>
